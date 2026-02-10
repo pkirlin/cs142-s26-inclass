4a6ecc894bcc33b21ecadd0b1d26893fbeeb3ac3
Phil Kirlin <kirlinp@rhodes.edu>
2026-02-10 15:27:57 -0600

    updates

---
+++ src/lab4/CarDemo.java
@@ -0,0 +1,10 @@
+package lab4;
+
+public class CarDemo {
+    public static void main(String[] args) {
+        // Let's get used to our new self-driving racecar class!
+        Car racecar = new Car();
+        System.out.println("Here are some details about our car: ");
+        System.out.println(racecar);
+    }
+}
