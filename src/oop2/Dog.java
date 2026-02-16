354a324c78df2efb2942861c25e98f49f6b9b9ec
Phil Kirlin <kirlinp@rhodes.edu>
2026-02-16 08:45:14 -0600

    updates

--- src/oop2/Dog.java
+++ src/oop2/Dog.java
@@ -2,12 +2,46 @@ package oop2;
 
 public class Dog {
     // fields or instance variables
-    String name;
-    int age;
+    private String name;
+    private int age;
+    private int energy;
+
+    public Dog() {
+        age = 1;
+        name = "My dog";
+    }
+
+    public Dog(String newName, int newAge) {
+        name = newName;
+        age = newAge;
+    }
 
     // methods
-    void birthday() {
+    public void birthday() {
         age++;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        if (age >= 0) {
+            this.age = age;
+        }
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public String toString() {
+        // return a String!
+        return "name of dog is " + name + " and age of dog is " + age;
     }
 
 }
--- src/oop2/DogDemo.java
+++ src/oop2/DogDemo.java
@@ -3,18 +3,18 @@ package oop2;
 public class DogDemo {
     public static void main(String[] args) {
         // create a new dog object
-        Dog fido = new Dog();
-        fido.name = "Fido";
-        fido.age = 5;
-        System.out.println("Fido's age is " + fido.age);
+        Dog fido = new Dog("Fido", 3);
+        //fido.setName("Fido");
+        //fido.setAge(11);
+        System.out.println("Fido's age is " + fido.getAge());
         fido.birthday();
-        System.out.println("Fido's age is " + fido.age);
+        System.out.println("Fido's age is " + fido.getAge());
 
         Dog toto = new Dog();
-        toto.name = "Toto";
-        toto.age = 3;
-        System.out.println("Toto's age is " + toto.age);
-
-
+        toto.setName("Toto");
+        //toto.age = 3;
+        System.out.println("Toto's age is " + toto.getAge());
+        System.out.println(toto);
+        System.out.println(fido);
     }
 }
