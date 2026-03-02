3c9535b216361f1a7ea80def77ac3238e5494cc0
Phil Kirlin <kirlinp@rhodes.edu>
2026-03-02 08:50:22 -0600

    updates

---
+++ src/prebreak/Exercises.java
@@ -0,0 +1,72 @@
+package prebreak;
+
+import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Exercises {
+
+    public static void main(String[] args) {
+        // Test noDuplicates:
+        ArrayList<Integer> intList = new ArrayList<Integer>();
+        intList.add(10);
+        intList.add(20);
+        intList.add(10);
+        intList.add(30);
+        intList.add(30);
+        intList.add(10);
+        intList.add(20);
+        intList.add(5);
+        System.out.println("ArrayList with duplicates: " + intList);
+        ArrayList<Integer> noDupes = noDuplicates(intList);
+        System.out.println("ArrayList with duplicates: " + noDupes);
+
+        // Test reverseString:
+        String s = "abcde";
+        String reversed = reverseString(s);
+        System.out.println("reversed string is " + reversed);
+
+    }
+
+    public static ArrayList<Integer> noDuplicates(ArrayList<Integer> list) {
+        return null;
+    }
+
+    public static String reverseString(String str) {
+        return "";
+    }
+
+    public static ArrayList<String> wordsContainingEveryVowel() {
+        return null;
+    }
+
+    public static ArrayList<String> wordsContainingEveryVowelExactlyOnce() {
+        return null;
+    }
+
+    public static ArrayList<String> lettersInAlphaOrder() {
+        return null;
+    }
+
+    public static ArrayList<String> findPalindromes() {
+        return null;
+    }
+
+    public static ArrayList<String> readWords() {
+        InputStream is = Exercises.class.getResourceAsStream("words.txt");
+        if (is == null) {
+            System.err.println("Could not open file: words.txt");
+            System.exit(1);
+        }
+        Scanner scan = new Scanner(is);
+
+        ArrayList<String> answer = new ArrayList<>();
+
+        while (scan.hasNextLine()) {
+            String line = scan.nextLine();
+            answer.add(line);
+        }
+
+        return answer;
+    }
+}
