9489342a603568eaf34d2fb3011188da3ea18b87
Phil Kirlin <kirlinp@rhodes.edu>
2026-03-30 07:58:35 -0500

    updates

---
+++ src/inherit1/Dog.java
@@ -0,0 +1,37 @@
+package inherit1;
+
+public class Dog
+{
+    private String name;
+    private int age;
+
+    public void speak() {
+        System.out.println(name + " says woof woof!");
+    }
+
+    public void chase(Dog otherdog) {
+        System.out.println(name + " chases " + otherdog.name);
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String newName) {
+        name = newName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int newAge) {
+        if (newAge >= 0) {
+            age = newAge;
+        }
+    }
+
+    public String toString() {
+        return "Dog object: name=" + name + " age=" + age;
+    }
+};
