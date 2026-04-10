b8458d1a28d736b3be75c4863dbcdd1d6f07b23c
Phil Kirlin <kirlinp@rhodes.edu>
2026-04-10 08:47:50 -0500

    updates

---
+++ src/poly1/ShowDog.java
@@ -0,0 +1,32 @@
+package poly1;
+
+public class ShowDog extends Dog {
+    private int numTrophies;
+
+    public ShowDog(String newName, int newAge) {
+        super(newName, newAge);
+        System.out.println("Constructing a showdog object.");
+    }
+
+    public int getNumTrophies() {
+        return numTrophies;
+    }
+
+    public void setNumTrophies(int numTrophies) {
+        this.numTrophies = numTrophies;
+    }
+
+    public void dance() {
+        System.out.println(getName() + " is dancing!");
+    }
+
+    public String toString() {
+        return "Showdog object: name=" + getName() + " age=" + getAge()
+                + " trophies=" + numTrophies;
+    }
+
+    public void speak() {
+        super.speak();
+        System.out.println("   and " + getName() + " also says woof woof in French.");
+    }
+}
